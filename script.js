56ca3e872bfad7d93c0e9dc4a5628aac2968ff04
berj ghazarian <ghazarian_berj@hotmail.com>
2022-03-09 15:01:13 -0800

    fixed moment time text format

--- script.js
+++ script.js
@@ -1,161 +1,157 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  console.log(moment());
+  console.log(moment().format());
 
-  console.log(moment())
-  console.log(moment().format())
+  $("#currentDay").text(moment().format("MMMM Do YYYY"))
 
-  $("#dateTime").text(moment().format("MMMM Do YYYY"))
-  
-  getTodo()
-  $(".saveBtn").on("click", function(event){
-      event.preventDefault()
-     
-      var todo = $("#9").val()
-      localStorage.setItem("todo", todo)
-      getTodo()
-  })
-
-  function getTodo(){
-      var storedTodos = localStorage.getItem("todo")
-      
-      $("#9").val(storedTodos) 
-  }
-
-  getTodoOne()
-  $(".saveBtn").on("click", function(event){
-      event.preventDefault()
-     
-      var todo = $("#10").val()
-      localStorage.setItem("todo1", todo)
-      getTodoOne()
-  })
-
-  function getTodoOne(){
-      var storedTodos = localStorage.getItem("todo1")
-      
-      $("#10").val(storedTodos) 
-  }
-
-  getTodoTwo()
-  $(".saveBtn").on("click", function(event){
-      event.preventDefault()
-     
-      var todo = $("#11").val()
-      localStorage.setItem("todo2", todo)
-      getTodoOne()
-  })
-
-  function getTodoTwo(){
-      var storedTodos = localStorage.getItem("todo2")
-      
-      $("#11").val(storedTodos) 
-  }
-
-  getTodoThree()
+  getTodo();
   $(".saveBtn").on("click", function (event) {
-      event.preventDefault()
+    event.preventDefault();
 
-      var todo = $("#12").val()
-      localStorage.setItem("todo3", todo)
-      getTodoThree()
-  })
+    var todo = $("#9").val();
+    localStorage.setItem("todo", todo);
+    getTodo();
+  });
+
+  function getTodo() {
+    var storedTodos = localStorage.getItem("todo");
+
+    $("#9").val(storedTodos);
+  }
+
+  getTodoOne();
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+
+    var todo = $("#10").val();
+    localStorage.setItem("todo1", todo);
+    getTodoOne();
+  });
+
+  function getTodoOne() {
+    var storedTodos = localStorage.getItem("todo1");
+
+    $("#10").val(storedTodos);
+  }
+
+  getTodoTwo();
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+
+    var todo = $("#11").val();
+    localStorage.setItem("todo2", todo);
+    getTodoOne();
+  });
+
+  function getTodoTwo() {
+    var storedTodos = localStorage.getItem("todo2");
+
+    $("#11").val(storedTodos);
+  }
+
+  getTodoThree();
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+
+    var todo = $("#12").val();
+    localStorage.setItem("todo3", todo);
+    getTodoThree();
+  });
 
   function getTodoThree() {
-      var storedTodos = localStorage.getItem("todo3")
+    var storedTodos = localStorage.getItem("todo3");
 
-      $("#12").val(storedTodos)
+    $("#12").val(storedTodos);
   }
 
-  getTodoFour()
+  getTodoFour();
   $(".saveBtn").on("click", function (event) {
-      event.preventDefault()
+    event.preventDefault();
 
-      var todo = $("#13").val()
-      localStorage.setItem("todo4", todo)
-      getTodoFour()
-  })
+    var todo = $("#13").val();
+    localStorage.setItem("todo4", todo);
+    getTodoFour();
+  });
 
   function getTodoFour() {
-      var storedTodos = localStorage.getItem("todo4")
+    var storedTodos = localStorage.getItem("todo4");
 
-      $("#13").val(storedTodos)
+    $("#13").val(storedTodos);
   }
 
-  getTodoFive()
+  getTodoFive();
   $(".saveBtn").on("click", function (event) {
-      event.preventDefault()
+    event.preventDefault();
 
-      var todo = $("#14").val()
-      localStorage.setItem("todo5", todo)
-      getTodoFive()
-  })
+    var todo = $("#14").val();
+    localStorage.setItem("todo5", todo);
+    getTodoFive();
+  });
 
   function getTodoFive() {
-      var storedTodos = localStorage.getItem("todo5")
+    var storedTodos = localStorage.getItem("todo5");
 
-      $("#14").val(storedTodos)
+    $("#14").val(storedTodos);
   }
 
-  getTodoSix()
+  getTodoSix();
   $(".saveBtn").on("click", function (event) {
-      event.preventDefault()
+    event.preventDefault();
 
-      var todo = $("#15").val()
-      localStorage.setItem("todo6", todo)
-      getTodoSix()
-  })
+    var todo = $("#15").val();
+    localStorage.setItem("todo6", todo);
+    getTodoSix();
+  });
 
   function getTodoSix() {
-      var storedTodos = localStorage.getItem("todo6")
+    var storedTodos = localStorage.getItem("todo6");
 
-      $("#15").val(storedTodos)
+    $("#15").val(storedTodos);
   }
 
-  getTodoSeven()
+  getTodoSeven();
   $(".saveBtn").on("click", function (event) {
-      event.preventDefault()
+    event.preventDefault();
 
-      var todo = $("#16").val()
-      localStorage.setItem("todo7", todo)
-      getTodoSeven()
-  })
+    var todo = $("#16").val();
+    localStorage.setItem("todo7", todo);
+    getTodoSeven();
+  });
 
   function getTodoSeven() {
-      var storedTodos = localStorage.getItem("todo7")
+    var storedTodos = localStorage.getItem("todo7");
 
-      $("#16").val(storedTodos)
+    $("#16").val(storedTodos);
   }
 
-  getTodoEight()
+  getTodoEight();
   $(".saveBtn").on("click", function (event) {
-      event.preventDefault()
+    event.preventDefault();
 
-      var todo = $("#17").val()
-      localStorage.setItem("todo8", todo)
-      getTodoEight()
-  })
+    var todo = $("#17").val();
+    localStorage.setItem("todo8", todo);
+    getTodoEight();
+  });
 
   function getTodoEight() {
-      var storedTodos = localStorage.getItem("todo8")
+    var storedTodos = localStorage.getItem("todo8");
 
-      $("#17").val(storedTodos)
+    $("#17").val(storedTodos);
   }
 
-  
-var time = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
-  
-function currentTime(){
-  var current = moment().format("H")
-  for (var i = 0; i < time.length; i++){
-  if(parseInt(time[i]) > current){
-      $("#" + time[i]).attr("style", "background-color: green")
+  var time = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+  function currentTime() {
+    var current = moment().format("H");
+    for (var i = 0; i < time.length; i++) {
+      if (parseInt(time[i]) > current) {
+        $("#" + time[i]).attr("style", "background-color: green");
+      } else if (parseInt(time[i]) == current) {
+        $("#" + time[i]).attr("style", "background-color: red");
+      } else if (parseInt(time[i]) < current) {
+        $("#" + time[i]).attr("style", "background-color: grey");
+      }
+    }
   }
-  else if(parseInt(time[i]) == current){
-      $("#" + time[i]).attr("style", "background-color: red")
-  }
-  else if(parseInt(time[i]) < current){
-      $("#" + time[i]).attr("style", "background-color: grey")
-  }
-  }
-}
-currentTime()
-})
+  currentTime();
+});
